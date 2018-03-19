7e3275ea31a731bf820e3045d640ea9c9948c53b
LoganLS <logan_56@hotmail.fr>
2018-03-19 09:08:27 +0100

    Modif sponsor + Controllers

--- WroomAdministrateur/router/router.js
+++ WroomAdministrateur/router/router.js
@@ -40,7 +40,7 @@ module.exports = function(app){
     app.get('/menuSponsors', SponsorController.menuSponsor);
     app.get('/menuSponsors/ajouterSponsor',SponsorController.pageAjouterSponsor);
     //app.post('/menuSponsors/ajouterSponsor/ajout',SponsorController.ajouterSponsor);
-    //app.get('/menuSponsors/modifierSponsor/:numSponsor',SponsorController.pageModifierSponsor);
+    app.get('/menuSponsors/modifierSponsor/:numSponsor',SponsorController.pageModifierSponsor);
     //app.post('/menuSponsors/modifierSponsor/:numSponsor/modifier',SponsorController.modifierSponsor);
     //app.get('/menuSponsors/supprimerSponsor/:numSponsor',SponsorController.pageSupprimerSponsor);
     
